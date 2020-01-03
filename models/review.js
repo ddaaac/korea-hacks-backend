4af4d98409c16399f0b890ebc3bc3a7cb4798fa0
BEOMJUN KIM <43440195+ddaaac@users.noreply.github.com>
2020-01-03 14:06:03 +0900

    feat: Implement Review collection

--- models/review.js
+++ models/review.js
@@ -1,24 +1,21 @@
-// models/review.js
-
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 
 let reviewSchema = mongoose.Schema({
     userId: mongoose.Schema.Types.ObjectId,
+    tagIds: [mongoose.Schema.Types.ObjectId],
     photos: [Buffer],                       // 한 도큐멘트의 최대 크기는 16MB
                                             // fs.readFileSync()로 읽어온 바이너리 그대로 저장하면 됨
     review: {
         type: String,
         required: [true, 'There should be any review'],     // 리뷰를 반드시 써야되는가?
     },
-    ratingList: [new mongoose.Schema({
-        rating: {
-            type: Number,
-            default: 0,
-            min: 0,
-            max: 5,
-            required: [true, 'You should rate for the review']
-        },
-    })],
-
+    views: {
+        type: Number,
+        default: 0,
+    },
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
 });
--- models/user.js
+++ models/user.js
@@ -1,5 +1,3 @@
-// models/user.js
-
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 
