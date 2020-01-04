c46f42c878cb065e69b6013db159e6badf2869f3
JJGwan926 <marong142@gmail.com>
2020-01-04 13:28:05 +0900

    feat: get reviews by a tag api

--- api/tags.js
+++ api/tags.js
@@ -6,8 +6,9 @@ const util = require('../util');
 // Get reviews of a tag
 router.get('/:tagId', util.isLoggedin, function (req, res, next) {
     Tag.findOne({_id: req.params.tagId})
-        .exec(function (err, tag) {
-            res.json(err || !tag ? util.successFalse(err) : util.successTrue(tag));
+        .populate('reviewIds').exec(function (err, tag) {
+            const reviews = tag.reviewIds;
+            res.json(err || !reviews ? util.successFalse(err) : util.successTrue(reviews));
         });
 });
 
