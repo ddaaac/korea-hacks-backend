7d075a18ab00fa05c876cc111ed8c56cbcdea239
wkd3475 <wkd3475@gmail.com>
2020-01-04 12:39:33 +0900

    feat: Add route

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ mongoose.connect(process.env.MONGO_DB_API, {
 app.use('/api/users', require('./api/users'));
 app.use('/api/auth', require('./api/auth'));
 app.use('/api/reviews', require('./api/reviews'));
+app.use('/api/evaluations', require('./api/evaluations'));
 app.use('/api/test', require('./api/test'));
 
 // [RUN SERVER]
