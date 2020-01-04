5b069ba77461db1e448bb858e80851e39b67fe96
wkd3475 <wkd3475@gmail.com>
2020-01-04 23:58:34 +0900

    fix: test

--- api/evaluations.js
+++ api/evaluations.js
@@ -34,7 +34,7 @@ router.get('/:reviewId/:gradePoint/:from', util.isLoggedin, function(req, res, n
         })
 });
 
-router.get('/:reviewId/newest/:from', util.isLoggedin, function(req, res, next) {
+router.get('/newest/:reviewId/:from', util.isLoggedin, function(req, res, next) {
     const from = parseInt(req.params.from);
     Evaluation.find({reviewId: req.params.reviewId})
         .sort('-updated_at')
@@ -44,7 +44,7 @@ router.get('/:reviewId/newest/:from', util.isLoggedin, function(req, res, next) 
         })
 })
 
-router.get('/:reviewId/recommend/:from', util.isLoggedin, function(req, res, next) {
+router.get('/recommend/:reviewId/:from', util.isLoggedin, function(req, res, next) {
     const from = parseInt(req.params.from);
     Evaluation.find({reviewId: req.params.reviewId})
         .sort('-gradePoint -updated_at')
